d773d5da9f700fc4dd2173f6aff7f90679420e55
Eftakhairul Islam <eftakhairul@gmail.com>
2014-11-10 21:43:32 -0500

    New test case updated

--- tests/RandomPasswordGeneratorTest.php
+++ tests/RandomPasswordGeneratorTest.php
@@ -8,11 +8,21 @@
  */
 class RandomPasswordGeneratorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCanBeNegated()
+    public function testIsReturnCorrectStringCount()
     {
-        // Arrange
+        //Create object
         $passwordGenerator =  new Eftakhairul\RandomPasswordGenerator();
 
         $this->assertEquals(9, strlen($passwordGenerator->useNumbers()->generatePassword(9) ));
+    }
+
+
+    public function testIsNumberContain()
+    {
+        //Create object
+        $passwordGenerator = new Eftakhairul\RandomPasswordGenerator();
+        $output            = preg_match("/[0-9]/i", $passwordGenerator->useNumbers(3)->generatePassword());
+
+        $this->assertEquals(true, $output);
     }
 }
